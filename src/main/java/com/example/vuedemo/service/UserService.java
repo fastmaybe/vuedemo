d3c4a46d3d9a077322949d124352c592b55fb23b
jianyuan <18672793611@163.com>
2019-01-24 15:01:25 +0800

    11111111111123

--- src/main/java/com/example/vuedemo/service/UserService.java
+++ src/main/java/com/example/vuedemo/service/UserService.java
@@ -1,4 +1,28 @@
 package com.example.vuedemo.service;
 
+import com.example.vuedemo.mapper.UserMapper;
+import com.example.vuedemo.pojo.Result;
+import com.example.vuedemo.pojo.User;
+import com.example.vuedemo.util.SnowFlake;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
 public class UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    public Result login(User user){
+        return null;
+    }
+
+    public Result add(User user) {
+        long id = SnowFlake.nextId();
+        user.setId(id);
+        System.out.println(id);
+        userMapper.insert(user);
+        System.out.println();
+        return null;
+    }
 }
